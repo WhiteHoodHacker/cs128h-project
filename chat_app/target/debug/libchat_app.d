b23b19a6a1d8d171d8c2dc7257dac58a1cd9b9b8
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-04-04 09:16:11 -0500

    Delete chat_app/target directory
    Removing local build from GitHub repository (build it locally to run)

--- chat_app/target/debug/libchat_app.d
+++
@@ -1,1 +0,0 @@
-/Users/dhirajk/Desktop/School/CS128H/FinalProject/cs128h-project/chat_app/target/debug/libchat_app.rlib: /Users/dhirajk/Desktop/School/CS128H/FinalProject/cs128h-project/chat_app/src/lib.rs
