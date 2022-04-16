77643e42747d2b7250a9e0bce41c2219ef507fa0
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-04-16 08:15:17 -0500

    successful post implementation

--- chat_app/src/lib.rs
+++ chat_app/src/lib.rs
@@ -1,22 +1,44 @@
 use rocket::FromForm;
 use rocket::serde::*;
-
+use chrono;
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[serde(crate="rocket::serde")]
+
+pub struct UserInput {
+    message: String,
+}
+impl UserInput {
+    pub fn to_message(&self) -> Message {
+        Message {
+            user_id: 1,
+            user_name: String::from("test"),
+            text: self.message.clone(),
+            timestamp: chrono::Utc::now(),
+        }
+    }
+}
 pub struct Message {
     pub user_id: i32,
     pub user_name: String,
     pub text: String,
-    pub timestamp: String,
+    pub timestamp: chrono::DateTime<chrono::Utc>,
 }
 impl Message {
+    pub fn new(user_id: i32, user_name: String, text: String) -> Message {
+        Message {
+            user_id: user_id,
+            user_name: user_name,
+            text: text,
+            timestamp: chrono::Utc::now(),
+        }
+    }
     pub fn as_string(&self) -> String {
         format!(
             "User Id: {}\nUser Name: {}\nText: {}\nDate-Time: {}",
             self.user_id,
             self.user_name,
             self.text,
-            self.timestamp
+            self.timestamp.with_timezone(&chrono::Local)
         )
     }
 }
--- chat_app/src/main.rs
+++ chat_app/src/main.rs
@@ -1,48 +1,20 @@
 #[macro_use]
 extern crate rocket;
 use chat_app::Message;
-use chrono;
+use chat_app::UserInput;
 use rocket::fs::{FileServer,relative};
-use rocket::response::stream::EventStream;
-use rocket::{State, Shutdown};
-use rocket::tokio::sync::broadcast::{channel, Sender};
+use rocket::tokio::sync::broadcast::{channel};
 use rocket::form::Form;
-use rocket::response::stream::Event;
-use rocket::tokio::select;
-#[get("/")]
-fn test() -> String {
-    let message = Message {
-        user_id: 1,
-        user_name: String::from("John"),
-        text: String::from("Hello, world!"),
-        timestamp: chrono::Utc::now().to_rfc2822(),
-    };
+
+#[post("/", data = "<form>")]
+fn post(form: Form<UserInput>)-> String {
+    let message: Message = form.into_inner().to_message();
     message.as_string()
-}
-#[post("/message", data = "<form>")]
-fn post(form: Form<Message>, queue: &State<Sender<Message>>){
-    let _temp = queue.send(form.into_inner());
-}
-#[get("/events")]
-async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
-    let mut rx = queue.subscribe();
-    EventStream! {
-        loop {
-            let msg = select! {
-                msg = rx.recv() => match msg {
-                    Ok(msg) => msg,
-                    Err(_) => break,
-                },
-                _ = &mut end => break,
-        };
-            yield Event::json(&msg);
-        }
-    }
 }
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .manage(channel::<Message>(10).0)
-    .mount("/", routes![post, events])
+    .manage(channel::<UserInput>(10).0)
+    .mount("/send", routes![post])
     .mount("/", FileServer::from(relative!("static")))
 }
